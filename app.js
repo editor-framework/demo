570ec45a8e405bba94a8e0cf19500db3a20fbb5b
Johnny Wu <KarasAya@gmail.com>
2016-09-26 18:03:58 +0800

    update to editor-framework 0.7.0

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ Editor.App.extend({
   },
 
   run () {
-    // create main window
-    let mainWin = new Editor.Window('main', {
+    Editor.run('app://index.html', {
       title: 'Editor Framework',
       width: 900,
       height: 700,
@@ -29,27 +28,5 @@ Editor.App.extend({
       show: false,
       resizable: true,
     });
-    Editor.Window.main = mainWin;
-
-    // restore window size and position
-    mainWin.restorePositionAndSize();
-
-    // load and show main window
-    // mainWin.nativeWin.once('ready-to-show', () => {
-    //   mainWin.show();
-    // });
-    mainWin.show();
-
-    // page-level test case
-    mainWin.load( 'app://index.html' );
-
-    // open dev tools if needed
-    if ( Editor.argv.showDevtools ) {
-      // NOTE: open dev-tools before did-finish-load will make it insert an unused <style> in page-level
-      mainWin.nativeWin.webContents.once('did-finish-load', function () {
-        mainWin.openDevTools();
-      });
-    }
-    mainWin.focus();
   },
 });
